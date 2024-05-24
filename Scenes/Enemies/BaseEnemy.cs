14c9a998631d8a9601293e16b29829a30342e756
AudreyL11 <alndstrm1@gmail.com>
2024-05-24 11:40:53 -0700

    initial commit

--- Scenes/Enemies/BaseEnemy.cs
+++ Scenes/Enemies/BaseEnemy.cs
@@ -23,58 +23,58 @@ public partial class BaseEnemy : PathFollow3D
 	{
 		if (ProgressRatio == 1)
 		{
-            AttachNextPath();
+			AttachNextPath();
 
 
-            //switch paths
-            //raycasting down (to hit the tile), asking for it's parent, then passing the tile into the chunk's thingy (GetPathsFromEntrance)
+			//switch paths
+			//raycasting down (to hit the tile), asking for it's parent, then passing the tile into the chunk's thingy (GetPathsFromEntrance)
 			//reparent to new path, set progress ratio to 0
-        }
+		}
 
-        Progress += 1.6f * (float)delta;
+		Progress += 1.6f * (float)delta;
 
-    }
+	}
 
-    public MeshInstance3D GetTileAt(Vector3 to, Vector3 from)
-    {
-        PhysicsDirectSpaceState3D spaceState = GetWorld3D().DirectSpaceState;
-        PhysicsRayQueryParameters3D query = PhysicsRayQueryParameters3D.Create(to, from, collisionMask: 8);
-        Dictionary result = spaceState.IntersectRay(query);
+	public MeshInstance3D GetTileAt(Vector3 to, Vector3 from)
+	{
+		PhysicsDirectSpaceState3D spaceState = GetWorld3D().DirectSpaceState;
+		PhysicsRayQueryParameters3D query = PhysicsRayQueryParameters3D.Create(to, from, collisionMask: 8);
+		Dictionary result = spaceState.IntersectRay(query);
 
-        if (result.Count > 1)
-        {
-            return ((StaticBody3D)result["collider"]).GetParent<MeshInstance3D>();
-        }
-        else
-        {
-            return null;
-        }
-    }
+		if (result.Count > 1)
+		{
+			return ((StaticBody3D)result["collider"]).GetParent<MeshInstance3D>();
+		}
+		else
+		{
+			return null;
+		}
+	}
 
-    public Chunk GetChunkReferenceFromTile(MeshInstance3D tile)
-    {
-        return tile.GetParent<Chunk>();
-    }
+	public Chunk GetChunkReferenceFromTile(MeshInstance3D tile)
+	{
+		return tile.GetParent<Chunk>();
+	}
 
-    public void AttachNextPath()
-    {
-        MeshInstance3D temp = GetTileAt(ToGlobal(new Vector3(0,1,0)), ToGlobal(new Vector3(0, -1, 0)));
-        Chunk chunk = GetChunkReferenceFromTile(temp);
-        Array<Path3D> paths = chunk.GetPathsFromEntrance(temp);
+	public void AttachNextPath()
+	{
+		MeshInstance3D temp = GetTileAt(ToGlobal(new Vector3(0,1,0)), ToGlobal(new Vector3(0, -1, 0)));
+		Chunk chunk = GetChunkReferenceFromTile(temp);
+		Array<Path3D> paths = chunk.GetPathsFromEntrance(temp);
 
-        if (paths != null)
-        {
-            Random random = new Random();
-            int randomIndex = random.Next(paths.Count);
-            Reparent(paths[randomIndex]);
-            ProgressRatio = 0;
-        } else
-        {
-            QueueFree();
-        }
+		if (paths != null)
+		{
+			Random random = new Random();
+			int randomIndex = random.Next(paths.Count);
+			Reparent(paths[randomIndex]);
+			ProgressRatio = 0;
+		} else
+		{
+			QueueFree();
+		}
 
-    }
-    
+	}
+	
 
 
 }
