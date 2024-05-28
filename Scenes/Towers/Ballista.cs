e7e3bba36af1b22bf606d3840520ea2d7af05058
Kristofer Koehn <fernandoprofitt@gmail.com>
2024-05-28 14:12:03 -0700

    added outline stuff

--- Scenes/Towers/Ballista.cs
+++ Scenes/Towers/Ballista.cs
@@ -1,24 +1,51 @@
 using Godot;
 using System;
 
+[Tool]
 public partial class Ballista : Node3D
 {
+
+	MeshInstance3D TowerBase;
 	MeshInstance3D BallistaMount;
 	MeshInstance3D BallistaBow;
     MeshInstance3D Arrow;
+	MeshInstance3D Outline;
 
+	StandardMaterial3D MouseOverOutline;
+	StandardMaterial3D SelectOutline;
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
 	{
-		BallistaMount = GetNode<MeshInstance3D>("weapon_ballista2");
+		MouseOverOutline = GD.Load<StandardMaterial3D>("res://Assets/Materials/MouseOverOutline.tres");
+		SelectOutline = GD.Load<StandardMaterial3D>("res://Assets/Materials/MouseOverOutline.tres");
+        TowerBase = GetNode<MeshInstance3D>("towerSquare_bottomA2/tmpParent/towerSquare_bottomA");
+		BallistaMount = GetNode<MeshInstance3D>("weapon_ballista2/tmpParent/weapon_ballista");
         BallistaBow = GetNode<MeshInstance3D>("weapon_ballista2/tmpParent/weapon_ballista/bow");
-		Arrow = GetNode<MeshInstance3D>("weapon_ballista2/tmpParent/weapon_ballista/bow/arrow");
-
+        Arrow = GetNode<MeshInstance3D>("weapon_ballista2/tmpParent/weapon_ballista/bow/arrow");
+		Outline = GetNode<MeshInstance3D>("Outline/MeshInstance3D");
     }
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+
+	}
+
+    public void _on_static_body_3d_mouse_entered()
+	{
+		Outline.Mesh.SurfaceSetMaterial(0, MouseOverOutline);
+		Outline.Visible = true;
+    }
+
+	public void _on_static_body_3d_mouse_exited()
+	{
+        Outline.Visible = false;
+    }
+
+
+    public void _on_static_body_3d_input_event(Node camera, InputEvent inputEvent, Vector3 position, Vector3 normal, int shape_idx) 
+	{
+
 	}
 }
