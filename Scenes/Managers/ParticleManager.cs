c10db6f5e2100d7ef7b32b26d61f0175f2c16984
AudreyL11 <alndstrm1@gmail.com>
2024-07-02 17:00:56 -0700

    updating particle system

--- Scenes/Managers/ParticleManager.cs
+++ Scenes/Managers/ParticleManager.cs
@@ -1,66 +1,89 @@
 using Godot;
 using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO.Enumeration;
 
 public partial class ParticleManager : Node
 {
 
-    private static ParticleManager instance;
+	private static ParticleManager instance;
 
-    //I think we are going to have to have multiple of the same particle effect unfortunately. While I can turn emitting on and off, it sounds like that will
-    //force the currently rendered particles to die and thats a MASSIVE bummer. so, we'll have multiple different particle lists. Will do when we have
-    //the emitters
-    public List<ParticleWrapper> Particles = new();
-    private ParticleSignals particleSignals = ParticleSignals.GetInstance();
+	//After messing around, doing an enumerable feels odd. New plan is to preload all our particles into a hashtable, then we can use that to nab the correct particle.
+	//From there, just instantiate it and youre good to go.
+	public List<ParticleWrapper> Particles = new();
+	private ParticleSignals particleSignals = ParticleSignals.GetInstance();
+	//private List<GpuParticles3D> loadedParticles = new();
+	private Hashtable loadedParticles = new Hashtable();
 
-    private ParticleManager() { }
+	private ParticleManager() { }
 
-    public static ParticleManager GetInstance()
-    {
-        if (instance == null)
-        {
-            instance = new ParticleManager();
-            SceneSwitcher.root.GetNode<GameLoop>("SceneSwitcher/GameLoop").AddChild(instance);
-            instance.Name = "ParticleManager";
-        }
-        return instance;
-    }
-
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
+	public static ParticleManager GetInstance()
 	{
-        particleSignals.p1 += (Vector3 c, Vector3 d) => makeP1(c, d);
-    }
+		if (instance == null)
+		{
+			instance = new ParticleManager();
+			SceneSwitcher.root.GetNode<GameLoop>("SceneSwitcher/GameLoop").AddChild(instance);
+			instance.Name = "ParticleManager";
+		}
+		return instance;
+	}
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		particleSignals.normalParticle += (String n, Vector3 c, Vector3 d) => makeNormalParticle(n, c, d);
+		//loading particles
+		foreach (string item in DirAccess.GetFilesAt("res://Scenes/Particles/"))
+		{
+			PackedScene inst = GD.Load<PackedScene>("res://Scenes/Particles/" + item + ".tscn");
+			//GpuParticles3D particleScene = inst.Instantiate<GpuParticles3D>();
+			loadedParticles[item] = inst;
+		}
+	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
 	}
 
-    //will have to have a few of these or a switcher to tell where to put it.
-    public void RegisterParticle(ParticleWrapper p)
-    {
-        Particles.Add(p);
-    }
+	//will have to have a few of these or a switcher to tell where to put it.
+	public void RegisterParticle(ParticleWrapper p)
+	{
+		Particles.Add(p);
+	}
 
-    public void UnregisterParticle(ParticleWrapper p)
-    {
-        Particles.Remove(p);
-    }
+	public void UnregisterParticle(ParticleWrapper p)
+	{
+		Particles.Remove(p);
+	}
 
-/*    private void fireP1(ParticleWrapper p, Vector3 c, Vector3 d)
-    {
-        p.emitEffect(c, d);
-    }*/
+	/*    private void fireP1(ParticleWrapper p, Vector3 c, Vector3 d)
+		{
+			p.emitEffect(c, d);
+		}*/
 
-    private void makeP1(Vector3 c, Vector3 d)
-    {
-        var inst = GD.Load<PackedScene>("res://Scenes/Particles/Particle1.tscn");
-        GpuParticles3D particleScene = inst.Instantiate<GpuParticles3D>();
-        AddChild(particleScene);
-        ParticleWrapper tempWraper = new ParticleWrapper(particleScene);
-        Particles.Add(tempWraper);
-        //fireP1(tempWraper, c, d);
-        tempWraper.emitEffect(c, d);
-    }
+	/*	private void makeP1(Vector3 c, Vector3 d)
+		{
+			var inst = GD.Load<PackedScene>("res://Scenes/Particles/Particle1.tscn");
+			GpuParticles3D particleScene = inst.Instantiate<GpuParticles3D>();
+			AddChild(particleScene);
+			ParticleWrapper tempWraper = new ParticleWrapper(particleScene);
+			Particles.Add(tempWraper);
+			//fireP1(tempWraper, c, d);
+			tempWraper.emitEffect(c, d);
+		}*/
+
+	private void makeNormalParticle(string name, Vector3 c, Vector3 d)
+	{ 
+		PackedScene inst = (PackedScene)loadedParticles[name];
+		if (inst != null)
+		{
+			GpuParticles3D particleScene = inst.Instantiate<GpuParticles3D>();
+			AddChild(particleScene);
+			ParticleWrapper tempWraper = new ParticleWrapper(particleScene);
+			Particles.Add(tempWraper);
+			tempWraper.emitEffect(c, d);
+		}
+	}
 }
--- Scenes/Particles/ParticleSignals.cs
+++ Scenes/Particles/ParticleSignals.cs
@@ -27,10 +27,15 @@ public partial class ParticleSignals : Node
 	{
 	}
 
-	public void dop1(Vector3 p, Vector3 r)
+	public void createParticle(String n, Vector3 p, Vector3 r)
+	{ 
+		this.EmitSignal("normalParticle", n, p, r);
+	}
+
+/*	public void dop1(Vector3 p, Vector3 r)
 	{
 		this.EmitSignal("p1", p, r);
-	}
+	}*/
 
 	public void initializeParticle(ParticleSignals signals)
 	{ 
@@ -39,8 +44,12 @@ public partial class ParticleSignals : Node
 
 
 	// Create as many of these as we end up having particle effects.
+	/*[Signal]
+	public delegate void p1EventHandler(Vector3 position, Vector3 degreeRotation);*/
+
+	//use this for non-following things, just put in necessary particle type.
 	[Signal]
-	public delegate void p1EventHandler(Vector3 position, Vector3 degreeRotation);
+	public delegate void normalParticleEventHandler(string n, Vector3 p, Vector3 r);
 
 
 }
